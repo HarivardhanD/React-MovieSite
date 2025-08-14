a839ee693b85de3de88dad4006f712ced8dfbf07
HarivardhanD <harivardhanmallya04@gmail.com>
2025-08-14 20:53:22 +0530

    movie-card component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { // funtion is component in react
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MovieCard movie={{title:"Tims film",release_date:"2024"}}/>
+    <MovieCard movie={{title:"Joes film",release_date:"2022"}}/>
+
+      
     </>
-  )
+  );
 }
 
 export default App
